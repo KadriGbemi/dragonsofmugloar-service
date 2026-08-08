557c276ff49ffa8f064e0c506fc2d1109708e33e
Gbemi <kadri.oluwagbemi@gmail.com>
2026-08-08 21:51:09 +0300

    deploy: fix base url for swagger on prod

--- swagger.config.ts
+++ swagger.config.ts
@@ -3,7 +3,7 @@ export const swaggerDoc = {
     title: "Dragons of Mugloar API",
     version: "1.0.0",
   },
-  host: "localhost:3000",
+  host: process.env.BACKEND_URL || "localhost:3000",
   schemes: ["http"],
 };
 
